8228fa5d56213dddf1e1ec89918123a7cb32dbe8
Sean Hart <31716085+seanhart23@users.noreply.github.com>
2021-11-07 21:17:22 -0500

    Minor fixes

--- src/components/partners/section-3/section-3.js
+++ src/components/partners/section-3/section-3.js
@@ -10,25 +10,25 @@ export class PSection3 extends React.Component {
             <div id='partner-section-3'>
                 <div className='container'>
                     <div class='content-container'>
-                        <h2>A single restaurant platform that works better together</h2>
+                        <h2>A single platform for all bars, pubs and clubs</h2>
                         <div class='row'>
                             <div class='col-lg-6'>
                                 <h4>Better for Guests</h4>
-                                <p>Delight your guests and increase spending by your regulars up to 30%.</p>
+                                <p>One app to discover all venues and enjoy new experiences</p>
                             </div>
                             <div class='col-lg-6'>
                                 <h4>Better for Operations</h4>
-                                <p>Simply run all orders through one point of sale and access 24/7 support.</p>
+                                <p>Manage venue operations from one location and save time</p>
                             </div>
                         </div>
                         <div class='row'>
                             <div class='col-lg-6'>
                                 <h4>Better for Employees</h4>
-                                <p>Help your team turn tables faster and run payroll in 30 minutes or less.</p>
+                                <p>Centralise information and let staff focus on delighting patrons</p>
                             </div>
                             <div class='col-lg-6'>
                                 <h4>Better for Business</h4>
-                                <p>Grow online sales and reduce third-party commissions by up to 80%.</p>
+                                <p>Know what works with data-driven insights from your patronage</p>
                             </div>
                         </div>
                     </div>
